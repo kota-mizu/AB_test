2c9d0ec98c591a75475e0797dae0b7a11b818a08
kota-mizu <kota.mizuno.en@gmail.com>
2024-10-23 12:01:03 +0900

    Update AB_test.py

--- AB_test.py
+++ AB_test.py
@@ -8,21 +8,6 @@ from scipy import stats
 
 sns.set(font_scale=2)
 
-# Everything is accessible via the st.secrets dict:
-
-st.write("DB username:", st.secrets["db_username"])
-st.write("DB password:", st.secrets["db_password"])
-st.write("My cool secrets:", st.secrets["my_cool_secrets"]["things_i_like"])
-
-# And the root-level secrets are also accessible as environment variables:
-
-import os
-
-st.write(
-    "Has environment variables been set:",
-    os.environ["db_username"] == st.secrets["db_username"],
-)
-
 # Google Fontsの読み込み
 st.markdown("""
     <style>
