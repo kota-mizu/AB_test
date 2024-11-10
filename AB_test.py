c68db7d9eb4ee3db0a68a1fea558687f73b92f91
kota-mizu <kota.mizuno.en@gmail.com>
2024-11-10 19:37:44 +0900

    Update AB_test.py

--- AB_test.py
+++ AB_test.py
@@ -175,7 +175,7 @@ if st.session_state.authenticated:
     st.markdown("""
 <div style="font-size: 14px; text-align: right;">
     <div style="display: inline-block; text-align: left; width: 50%;">※増加差分 = （BのCVR - AのCVR）×（A+Bの総訪問者数）</div><br>
-    <div style="display: inline-block; text-align: left; width: 50%;">※月間換算 = 増加差分 ÷ テスト日数 × 30日</div>
+    <div style="display: inline-block; text-align: left; width: 50%;">※月間換算 = （増加差分）÷（テスト日数）×（30日）</div>
 </div>
 """, unsafe_allow_html=True)
 
